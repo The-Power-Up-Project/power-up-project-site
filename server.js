a5314d98c55c97d6bdc9690518256d689195d1f6
jackkrzyz <jackkrzyz@gmail.com>
2025-12-01 21:23:51 -0600

    update callback url

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://thepowerupproject.org/auth/google/callback"
+    callbackURL: "https://thepowerupproject.org/auth/google/callback"
   }, (accessToken, refreshToken, profile, done) => {
     return done(null, profile);
   }
